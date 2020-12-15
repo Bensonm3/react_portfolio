e9bee56d1fff86b47b59e14cf7a7bfa2ecd979a8
Matt Benson <rabenson6@msn.com>
2020-12-14 20:49:51 -0800

    updated portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -25,12 +25,17 @@ const Portfolio = () => {
             </div>
             <div class="row row2b">
                 <div class="col-md-6 picborder"> 
+                
                     <div class="picbox">
                             <img class="pic pic1" src={getOutside} alt="pic 1" />
                             <div class="picbanner">
                                 <a href="https://bmalkmus.github.io/getOutsideApp/" >Get Outside App </a>  
-                                <a class="github" href="https://github.com/bmalkmus/getOutsideApp" >Github</a> 
-                                
+                                <a class="github" href="https://github.com/bmalkmus/getOutsideApp" >Github</a>   
+                            </div>
+                            <div class="picbanner2">
+                                <a>Plan a day trip to a Seattle Park and restaurant</a>
+                                <br></br>
+                                <a>Javascript, Seattle Parks API, Zomato API, Materialize CSS</a>
                             </div>
                     </div>
                 </div>
@@ -40,6 +45,11 @@ const Portfolio = () => {
                         <div class="picbanner"> 
                             <a href=" https://bensonm3.github.io/hw6/" >Weather App </a> 
                             <a class="github" href="https://github.com/Bensonm3/Weather-Dashboard" >Github</a>
+                        </div>
+                        <div class="picbanner2">
+                                <a>Weather App with current and searchable forecast data.</a>
+                                <br></br>
+                                <a>Javascript, HTML, CSS, jQuery, Open Weather API</a>
                         </div>
                     </div>
                 </div>
@@ -52,14 +62,24 @@ const Portfolio = () => {
                                 <a href="https://bensonm3.github.io/hw4/quiz.html" >Code Quiz </a> 
                                 <a class="github" href="https://github.com/Bensonm3/Coding-Quiz-Game" >Github</a>
                             </div>
+                            <div class="picbanner2">
+                                <a>Coding Acroynm Quiz Game.</a>
+                                <br></br>
+                                <a>Javascript, HTML, CSS</a>
+                            </div>
                         </div>
                 </div>
                 <div class="col-md-6 picborder">
                     <div class="picbox">
                             <img class="pic pic4" src={fairweather} alt="pic 4" />
                             <div class="picbanner"> 
-                                <a href="https://fair-weather-hiking.herokuapp.com/" >Fair Weather Hiker </a>
+                                <a class="fairWeather" href="https://fair-weather-hiking.herokuapp.com/" >Hiking App </a>
                                 <a class="github" href="https://github.com/doctorbebo/Fair_Weather_Hiking_App" >Github</a>
+                            </div>
+                            <div class="picbanner2">
+                                <a>Fitness App with hike search by weather conditions.</a>
+                                <br></br>
+                                <a>React, Javascript, MongoDB, Mongoose, Redux, Materialize CSS</a>
                             </div>
                     </div>
                 </div>
@@ -69,8 +89,13 @@ const Portfolio = () => {
                     <div class="picbox">
                             <img class="pic pic5" src={oat} alt="pic 5" />
                             <div class="picbanner"> 
-                                <a href="https://secret-wildwood-65776.herokuapp.com/" >Order Allocation Tool </a>
+                                <a href="https://secret-wildwood-65776.herokuapp.com/" >OAT </a>
                                 <a class="github" href="https://github.com/esteinmetzer/reborn-badass" >Github</a>
+                            </div>
+                            <div class="picbanner2">
+                                <a>Order Allocation Tool for supply chain management.</a>
+                                <br></br>
+                                <a>SQL, Express.js, Javascript, HTML, CSS</a>
                             </div>
                         </div>
                     </div>
@@ -81,6 +106,11 @@ const Portfolio = () => {
                             <a href="https://bensonm3.github.io/Day-Planner/" >Day Planner </a>
                             <a class="github"href="https://github.com/Bensonm3/Day-Planner" >Github</a>
                                 </div>
+                                <div class="picbanner2">
+                                <a>Day Calendar with active time keeping.</a>
+                                <br></br>
+                                <a>jQuery, Javascript, Moment.js, HTML, CSS</a>
+                            </div>
                         </div>
                     </div>
                 </div>
